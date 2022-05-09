ebbbcd822469c8637482115ad823c43ab57545f9
ciko <git@ciko.io>
2022-05-09 21:57:54 +0200

    Added csv output for dump_db

--- dump_db.py
+++ dump_db.py
@@ -6,11 +6,19 @@ import sqlite3
 parser = argparse.ArgumentParser(description="Dump the domainsquatting database")
 parser.add_argument("dbfile", type=str, help="Squatting database to load")
 parser.add_argument("--filter", nargs="+", type=str, help="Filter master names containing these strings")
+parser.add_argument("--csv", action="store_true", help="Output as csv")
 args = parser.parse_args()
 
 # Setup the database
 con = sqlite3.connect(args.dbfile)
 cur = con.cursor()
+
+def print_csv(row):
+	print(f"{row[0]}.{row[1]};{row[2]};{row[-1]}")
+
+def print_plain(row):
+	print(f"{row[0]}.{row[1]}\t{row[2]}\t{row[-1]}")
+
 for row in cur.execute("SELECT * FROM domains"):
 	filtered = False
 	if args.filter:
@@ -20,6 +28,9 @@ for row in cur.execute("SELECT * FROM domains"):
 				break
 
 	if not filtered:
-		print(f"{row[0]}.{row[1]}\t{row[2]}\t{row[-1]}")
+		if args.csv:
+			print_csv(row)
+		else:
+			print_plain(row)
 
 con.close()
